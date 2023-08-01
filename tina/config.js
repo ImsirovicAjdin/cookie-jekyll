6062ac7f77b1e41a49da53848b9d31d0ef286219
Ajdin Imsirovic <ajdika@live.com>
2023-08-01 05:39:57 +0200

    Add tina creds

--- tina/config.js
+++ tina/config.js
@@ -5,8 +5,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
-  clientId: null, // Get this from tina.io
-  token: null, // Get this from tina.io
+  clientId: "53b2caa6-ea59-40ba-ad0a-582bc8a15a98", // Get this from tina.io
+  token: "872997069adc4ce22f9401becc029ee3ecf1fc16", // Get this from tina.io
 
   build: {
     outputFolder: "admin",
